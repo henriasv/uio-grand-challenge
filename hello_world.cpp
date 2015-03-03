5e519a575bd55cfb53ccba4614cbcd2d7b9feab7
Henrik Sveinsson <henrik.sveinsson@me.com>
2015-03-03 14:26:23 +0100

    updated example

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string>
+
+void sayHello(std::string name);
+
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	sayHello("Thomas");
+
+	for (int i = 0; i<10; i++)
+	{
+		std::cout << i << std::endl;
+	}
+
+	int counter = 0;
+	while (counter<10)
+	{
+		std::cout << counter << std::endl;
+		counter ++;
+	}
+
+}
+
+
+void sayHello(std::string name)
+{
+	std::cout << "Hello " << name << std::endl;
 }
