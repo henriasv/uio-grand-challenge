42535ad0af12f91c0e778434e4ff9f94078182fc
Henrik Sveinsson <henrik.sveinsson@me.com>
2016-03-30 09:57:16 +0200

    Rewrote analysis script

--- readResults.py
+++ readResults.py
@@ -2,17 +2,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = "output/positions.bin"
 
-numBlocks 	= 70;
+class FrictionAnalyser:
+    def __init__(self, parameterFileName):
+        self.parameterFileName = parameterFileName
+        self.readParameters()
 
-positions = np.fromfile(filename)
-nt = len(positions)/numBlocks;
+    def readParameters(self):
+        ifile = open(self.parameterFileName, "r")
+        fileContents = ifile.readlines()
+        for line in fileContents:
+            words = line.split()
+            if words[0] == "nx":
+                self.nx = float(words[1])
+            if words[0] == "dt":
+                self.dt = float(words[1])
+            # TODO: get more parameters from parameter file
 
-positions.resize(nt, numBlocks)
+    def pcolorplot(self, spaceTimeFile):
+        spaceTimeData = np.fromfile(spaceTimeFile)
+        nt = len(spaceTimeData)/self.nx;
+        spaceTimeData.resize(nt, self.nx)
+        plt.pcolormesh(spaceTimeData-spaceTimeData[0])
+        plt.xlabel('Block index')
+        plt.ylabel('Time step')
+        plt.colorbar()
+        plt.show()
 
-plt.pcolormesh(positions-positions[0])
-plt.xlabel('Block index')
-plt.ylabel('Time step')
-plt.colorbar()
-plt.show()
+if __name__=="__main__":
+    filenameParameters = "output/parameters.txt"
+    filenamePositions = "output/positions.bin"
+    analyser = FrictionAnalyser(filenameParameters)
+    analyser.pcolorplot(filenamePositions)
